8908cc4836602105de610ee3557aa3ada11771e9
Juliano Dorneles dos Reis <juliano.reis@nave.rs>
2021-10-11 00:22:22 -0300

    feat(font-family) add font-family files

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,8 @@ import type { AppProps } from 'next/app'
 
 import Theme from '@theme'
 
+import '../../public/fonts/fonts.css'
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Theme>
